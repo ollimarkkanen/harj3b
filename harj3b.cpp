a4872641b763df8eec47304b4f135734b3e7382b
ollimarkkanen <markkanen.olli@gmail.com>
2014-09-15 21:10:37 +0300

    Create harj3b.cpp

---
+++ harj3b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void main(void)
+{
+	cout << "**********************************\n";
+	cout << "* *\n";
+	cout << "* Olli Markkanen *\n";
+	cout << "* *\n";
+	cout << "*-------------------------------------- *\n";
+	cout << "* *\n";
+	cout << "* Sammonkatu 3 A 10 *\n";
+	cout << "* 44100 Aanekoski\n";
+	cout << "* *\n";
+	cout << "**********************************\n";
+	cin.get();
+}
